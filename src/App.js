c1bd048b1d8b957845673d49804535be98f4cfb2
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-07 15:41:13 +0200

    open button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+//data
+import data from "./data";
+
 // Comp
 import Card from "./comp/card";
+
+const questionsList = data.map((item) => <Card {...item} />);
 
 function App() {
   return (
     <div className="app">
       <h1 className="app--title">Questions and Answers About Login</h1>
-      <Card />
+      <div className="app--ques--list">{questionsList}</div>
     </div>
   );
 }
--- src/comp/card.js
+++ src/comp/card.js
@@ -2,16 +2,30 @@ import React from "react";
 
 //assets
 import icon from "../assets/icon.png";
+import icon1 from "../assets/icon1.png";
 
-const Card = () => (
-  <div className="card">
-    <div className="card--ques">
-      <h1 className="card--ques-text">Card Hello World</h1>
-      <button className="card--ques--but">
-        <img className="card--ques--img" src={icon} alt="expand img" />
-      </button>
+const Card = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  function expand() {
+    setOpen(!open);
+  }
+
+  return (
+    <div className="card">
+      <div className="card--ques">
+        <h1 className="card--ques--text">{props.title}</h1>
+        <button className="card--ques--but" onClick={expand}>
+          <img
+            className="card--ques--img"
+            src={open ? icon1 : icon}
+            alt="expand img"
+          />
+        </button>
+      </div>
+      <div className="card--ans">{open?props.info:""}</div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
